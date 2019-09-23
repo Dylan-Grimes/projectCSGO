fcf701139662b3b1c95eca5590c7d71235fac25e
dylan <dylan.grimes@academytrainee.com>
2019-09-23 20:44:42 +0100

    Added get all teams to teamscontroller

--- src/main/java/com/qa/controllers/TeamsController.java
+++ src/main/java/com/qa/controllers/TeamsController.java
@@ -1,4 +1,22 @@
 package com.qa.controllers;
 
+import com.qa.models.Teams;
+
+import com.qa.repository.TeamsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin()
 public class TeamsController {
+
+    @Autowired
+    private TeamsRepository repository;
+
+    @RequestMapping(value = "teams", method = RequestMethod.GET)
+    public List<Teams> listAllTeams() {
+        return repository.findAll();
+    }
 }
