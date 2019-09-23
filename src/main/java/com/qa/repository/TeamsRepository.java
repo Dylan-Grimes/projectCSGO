ecab82f77f5087bd3d9a42d6e27c6f624031f2e6
dylan <dylan.grimes@academytrainee.com>
2019-09-23 20:34:35 +0100

    Added teams repository class

---
+++ src/main/java/com/qa/repository/TeamsRepository.java
@@ -0,0 +1,8 @@
+package com.qa.repository;
+
+import com.qa.models.Teams;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamsRepository extends JpaRepository<Teams, Long> {
+
+}
