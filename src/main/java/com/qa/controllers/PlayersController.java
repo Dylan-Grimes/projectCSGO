c2cb5947add52d5d40d984aaafaf4c07401e93f7
dylan <dylan.grimes@academytrainee.com>
2019-09-23 20:35:29 +0100

    Added get all players method to controller

--- src/main/java/com/qa/controllers/PlayersController.java
+++ src/main/java/com/qa/controllers/PlayersController.java
@@ -1,4 +1,22 @@
 package com.qa.controllers;
 
+import com.qa.models.Players;
+import com.qa.repository.PlayersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin()
 public class PlayersController {
+
+    @Autowired
+    private PlayersRepository repository;
+
+    @RequestMapping(value = "players", method = RequestMethod.GET)
+    public List<Players> listAllPlayers() {
+        return repository.findAll();
+    }
+
 }
